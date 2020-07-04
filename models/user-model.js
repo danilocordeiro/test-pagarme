8cf35e626c7df0ada1ce694e00f0f6ea2bb28035
danilocordeiro.dev@gmail.com <danilocordeiro.dev@gmail.com>
2020-07-04 00:04:23 -0300

    Add user model

---
+++ models/user-model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const { ObjectId, Schema } = mongoose.Schema
+
+const userModel = new Schema(
+  {
+    name: { trim: true, index: true, required: true, type: String },
+    email: { type: String },
+    password: { type: String },
+    active: { type: Boolean, required: true, default: true },
+    createdAt: { type: Date, default: Date.now }
+  },
+  { versionkey: false }
+)
+
+userModel.pre('save', (next) => {
+  const now = new Date()
+  if (!this.createdAt) this.createdAt = now
+  next()
+})
+
+module.exports = mongoose.model('User', userModel)
