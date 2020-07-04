b0ff04e1cb4c2aaaed78925d40c4d60909b84198
danilocordeiro.dev@gmail.com <danilocordeiro.dev@gmail.com>
2020-07-04 20:22:11 -0300

    add transaction and card route

---
+++ routes/card-router.js
@@ -0,0 +1,10 @@
+const router = require('express').Router()
+const Controller = require('../controllers/card-controller')
+const auth = require('../middlewares/authentication')
+const _ctrl = new Controller()
+
+router.get('/', auth, _ctrl.get)
+router.post('/', auth, _ctrl.post)
+router.put('/:id', auth, _ctrl.delete)
+
+module.exports = router
